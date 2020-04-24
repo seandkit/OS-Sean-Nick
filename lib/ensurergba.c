add926e413424b7fe8c99d28464526e668a9e229
Sean I <D00191063@student.dkit.ie>
2020-04-24 10:47:00 +0000

    Update to Section 2 answers to correct errors

--- lib/ensurergba.c
+++ lib/ensurergba.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main () {
 
 	char* line;
 	size_t length = 0;
+	char buffer[2];
 	while (getline(&line, &length, stdin) != -1) {
 
 		if (strlen(line) == 8) {
-			printf("%.*sFF\n", (int)strlen(line) - 1, line);
+			//printf("%.*sFF\n", (int)strlen(line) - 1, line);
+			memcpy(buffer, &line[1], 2);
+			int r = strtol(buffer, NULL, 16);
+			memcpy(buffer, &line[3], 2);
+			int g = strtol(buffer, NULL, 16);
+			memcpy(buffer, &line[5], 2);
+			int b = strtol(buffer, NULL, 16);
+			printf("rgba(%d,%d,%d,255)\n", r, g, b);
 		}
-		else {
-			printf("%s", line);
+		else if (strlen(line) == 10){
+			//printf("%s", line);
+			memcpy(buffer, &line[1], 2);
+			int r = strtol(buffer, NULL, 16);
+			memcpy(buffer, &line[3], 2);
+			int g = strtol(buffer, NULL, 16);
+			memcpy(buffer, &line[5], 2);
+			int b = strtol(buffer, NULL, 16);
+			memcpy(buffer, &line[7], 2);
+			int a = strtol(buffer, NULL, 16);
+			printf("rgba(%d,%d,%d,%d)\n", r, g, b, a);
 		}
 	}
 
